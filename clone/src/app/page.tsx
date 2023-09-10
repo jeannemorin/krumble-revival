c534c3085b68942f1c200c1974f115193c9f50f3
jeannemorin <46577907+jeannemorin@users.noreply.github.com>
2023-09-10 16:50:50 +0200

    fetching listings data ok

--- clone/src/app/page.tsx
+++ clone/src/app/page.tsx
@@ -1,8 +1,42 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import ClientOnly from './components/ClientOnly'
+import Container from './components/Container'
+import EmptyState from './components/EmptyState';
+import getListings from './actions/getListings';
 
-export default function Home() {
+export default async function Home() {
+  const listings = await getListings();
+
+  if (listings.length === 0) {
+    return (
+      <Container>
+          <EmptyState showReset />
+      </Container>
+    )
+  }
   return (
-    <div className='text-blue-500'>Hello Clone</div>
+    <ClientOnly>
+      <Container>
+        <div className='
+        pt-24 
+        grid 
+        grid-cols-1 
+        sm:grid-cols-2 
+        md:grid-cols-3 
+        lg:grid-cols-4 
+        xl:grid-cols-5 
+        2xl:grid-cols-6
+        gap-8'>
+          <div>
+            {listings.map((listing: any) => {
+              return (
+                <div>{listing.title}</div>
+              )
+            })}
+          </div>  
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
